891452fa1915de86ee3e3cf92fe732f14bc5b4a4
saim-jamil1 <63855149+saim-jamil1@users.noreply.github.com>
2020-12-24 20:02:29 +0500

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+const KActiveCardColor = Color(0xFF1D1E33);
+const KButtonColor = Color(0xFFEB1555);
+const KInactiveCardColor= Color(0xFF111328);
+
+const TextStyle KLabelText = TextStyle(
+  fontSize: 19.0,
+  color: Color(0xFF8D8E98),
+);
+
+const TextStyle KNumberStyle =  TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const KButtonCalculateSTyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const KResulTitle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const KResultext = TextStyle(
+  color: Color(0xFF24d876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+const KBmiValueStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const KbmiInterpretStyle = TextStyle(
+  fontSize: 22.0,
+
+);
